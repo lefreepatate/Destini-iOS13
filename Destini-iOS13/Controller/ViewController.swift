0976394b1dab5204137a0b12968f4a8d96b6e41f
lefreepatate <c_garcia_muskat@hotmail.com>
2020-04-10 11:38:19 +0200

    Refactoring and using choiceXDestination to get clearer code

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -14,44 +14,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     @IBAction func choiceMade(_ sender: UIButton) {
-        updateUI(userChoice: sender.tag)
-        print(sender.tag)
+        storyBrain.nextStory(userChoice: sender.currentTitle!)
+        updateUI()
     }
-    let storyBrain = StoryBrain()
+    var storyBrain = StoryBrain()
     override func viewDidLoad() {
         super.viewDidLoad()
-        updateStory(story: 0)
+        updateUI()
     }
-    func updateStory(story: Int) {
-        let storyLabels = storyBrain.nextStory(numberStory: story)
-        storyLabel.text = storyLabels[0]
-        choice1Button.setTitle("\(storyLabels[1])", for: .normal)
-        choice2Button.setTitle(storyLabels[2], for: .normal)
-    }
-    func updateUI(userChoice: Int) {
-        if storyLabel.text == storyBrain.stories[0].title {
-            if userChoice == 0 {
-                updateStory(story: 2)
-            } else if userChoice == 1 {
-                updateStory(story: 1)
-            }
-        } else if storyLabel.text == storyBrain.stories[1].title {
-            if userChoice == 0 {
-                updateStory(story: 2)
-            } else if userChoice == 1 {
-                updateStory(story: 3)
-            }
-        } else if storyLabel.text == storyBrain.stories[2].title {
-            if userChoice == 0 {
-                updateStory(story: 5)
-            } else if userChoice == 1 {
-                updateStory(story: 4)
-            }
-        } else if storyLabel.text == storyBrain.stories[3].title || storyLabel.text == storyBrain.stories[4].title || storyLabel.text == storyBrain.stories[5].title {
-            if userChoice == 0 || userChoice == 1 {
-                updateStory(story: 0)
-            }
-        }
+ 
+    func updateUI() {
+        storyLabel.text = storyBrain.getTitleStory()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
     }
 }
 
