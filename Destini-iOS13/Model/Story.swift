46a0cf248104724b49afe14208ab544df798cfce
lefreepatate <c_garcia_muskat@hotmail.com>
2020-04-10 11:15:32 +0200

    Adding new stories to check the excercice project and MVC

--- Destini-iOS13/Model/Story.swift
+++ Destini-iOS13/Model/Story.swift
@@ -12,10 +12,16 @@ struct Story {
     let title: String
     let choice1: String
     let choice2: String
+    let choice1Destination: Int
+    let choice2Destination: Int
     
-    init(title: String, c1: String, c2: String) {
+    init(title: String, choice1: String, choice1Destination: Int, choice2: String, choice2Destination: Int) {
         self.title = title
-        choice1 = c1
-        choice2 = c2
+        self.choice1 = choice1
+        self.choice2 = choice2
+        self.choice1Destination = choice1Destination
+        self.choice2Destination = choice2Destination
     }
+
+     
 }
